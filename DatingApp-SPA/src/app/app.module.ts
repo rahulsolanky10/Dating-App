0ca5f882fb8a314752b2fb1a1cf5355d5645a728
rahulsolanky10 <monusolanky105@gmail.com>
2020-05-03 10:29:02 +0530

    End of section 7

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -10,15 +10,30 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     BsDropdownModule.forRoot(),
     HttpClientModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
+
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent],
 })
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router ) { }
 
   ngOnInit() {
   }
@@ -22,6 +23,9 @@ export class NavComponent implements OnInit {
 
    }, error => {
      this.alertify.error('Failed');
+   }, () =>
+   {
+     this.router.navigate(['/members']);
    }
    ) ;
   }
@@ -34,6 +38,7 @@ export class NavComponent implements OnInit {
   {
   localStorage.removeItem('token');
   this.alertify.message('logout');
+  this.router.navigate(['/home']);
   }
 
 
