f840eaabb10ed0e9afe522709518d2847800bb7e
rahulsolanky10 <monusolanky105@gmail.com>
2020-05-04 12:10:37 +0530

    End

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -19,6 +19,9 @@ using System.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Diagnostics;
 using DatingApp.API.Helpers;
+using AutoMapper;
+
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
 
 namespace DatingApp.API
 {
@@ -35,8 +38,14 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         { 
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers().AddNewtonsoftJson(
+                opt => {
+                    opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
             services.AddControllers();
+            services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddScoped<IAuthRepository,AuthRepository>();
+            services.AddScoped<IDatingRepository,DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options => 
             options.TokenValidationParameters= new TokenValidationParameters
